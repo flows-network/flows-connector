bce65136033d7233418f5c536a9dce349f60a210
chen982 <1366192373@qq.com>
2022-08-16 22:08:25 +0800

    add jira

--- jira/src/main.rs
+++ jira/src/main.rs
@@ -25,16 +25,17 @@ lazy_static! {
 	static ref REACTOR_API_PREFIX: String = env::var("REACTOR_API_PREFIX").expect("Env variable REACTOR_API_PREFIX not set");
 	static ref REACTOR_AUTH_TOKEN: String = env::var("REACTOR_AUTH_TOKEN").expect("Env variable REACTOR_AUTH_TOKEN not set");
 
-	static ref GITLAB_APP_CLIENT_ID: String = env::var("GITLAB_APP_CLIENT_ID").expect("Env variable GITLAB_APP_CLIENT_ID not set");
-	static ref GITLAB_APP_CLIENT_SECRET: String = env::var("GITLAB_APP_CLIENT_SECRET").expect("Env variable GITLAB_APP_CLIENT_SECRET not set");
-	static ref GITLAB_REDIRECT_URL: String = env::var("GITLAB_REDIRECT_URL").expect("Env variable GITLAB_REDIRECT_URL not set");
+	static ref JIRA_APP_CLIENT_ID: String = env::var("JIRA_APP_CLIENT_ID").expect("Env variable JIRA_APP_CLIENT_ID not set");
+	static ref JIRA_APP_CLIENT_SECRET: String = env::var("JIRA_APP_CLIENT_SECRET").expect("Env variable JIRA_APP_CLIENT_SECRET not set");
+	static ref JIRA_REDIRECT_URL: String = env::var("JIRA_REDIRECT_URL").expect("Env variable JIRA_REDIRECT_URL not set");
 	static ref SERVICE_API_PREFIX: String = env::var("SERVICE_API_PREFIX").expect("Env var SERVICE_API_PREFIX not set");
 
-	static ref RSA_RAND_SEED: [u8; 32] = env::var("RSA_RAND_SEED")
-        .expect("Env variable RSA_RAND_SEED not set")
-        .as_bytes()
-        .try_into()
-        .unwrap();
+	// static ref RSA_RAND_SEED: [u8; 32] = env::var("RSA_RAND_SEED")
+    //     .expect("Env variable RSA_RAND_SEED not set")
+    //     .as_bytes()
+    //     .try_into()
+    //     .unwrap();
+	static ref RSA_RAND_SEED: [u8; 32] = [8;32];
 	
     static ref CHACHA8RNG: ChaCha8Rng = ChaCha8Rng::from_seed(*RSA_RAND_SEED);
     static ref PRIV_KEY: RsaPrivateKey =
@@ -87,14 +88,17 @@ async fn auth(Query(auth_body): Query<AuthBody>) -> impl IntoResponse {
 					Ok(gu) => {
 						let encrypted = serde_json::json!({
 							"access_token":&at.access_token,
+							"id":&gu.0
 						});
 						let location = format!(
 							"{}/api/connected?authorId={}&authorName={}&authorState={}&refreshState={}",
 							REACTOR_API_PREFIX.as_str(),
 							gu.0,
 							gu.1,
-							encrypt(&serde_json::to_string(&encrypted).unwrap()),
-							encrypt(&at.refresh_token)
+							serde_json::to_string(&encrypted).unwrap(),
+							at.refresh_token
+							// encrypt(&serde_json::to_string(&encrypted).unwrap()),
+							// encrypt(&at.refresh_token)
 						);
 						Ok((StatusCode::FOUND, [("Location", location)]))
 					}
@@ -120,16 +124,18 @@ struct OAuthBody {
 
 async fn get_access_token(code: &str) -> Result<OAuthBody, String> {
 
+	println!("{}",code);
+
 	let params = [
-		("client_id", GITLAB_APP_CLIENT_ID.as_str()), 
-		("client_secret", GITLAB_APP_CLIENT_SECRET.as_str()),
+		("client_id", JIRA_APP_CLIENT_ID.as_str()), 
+		("client_secret", JIRA_APP_CLIENT_SECRET.as_str()),
 	    ("grant_type", "authorization_code"),
 		("code", &code),
-		("redirect_uri", GITLAB_REDIRECT_URL.as_str()),
+		("redirect_uri", JIRA_REDIRECT_URL.as_str()),
 	];
 
 	let response = HTTP_CLIENT
-		.post("https://gitlab.com/oauth/token")
+		.post("https://auth.atlassian.com/oauth/token")
 		.form(&params)
 		.send()
 		.await;
@@ -154,10 +160,10 @@ async fn get_access_token(code: &str) -> Result<OAuthBody, String> {
 
 async fn get_authed_user(access_token: &str) -> Result<(String, String), String> {
 
-	// println!("{}",access_token);
+	println!("{}",access_token);
 
 	let response = HTTP_CLIENT
-		.get("https://gitlab.com/api/v4/user")
+		.get("https://api.atlassian.com/oauth/token/accessible-resources")
 		.bearer_auth(access_token)
 		.send()
 		.await;
@@ -167,8 +173,8 @@ async fn get_authed_user(access_token: &str) -> Result<(String, String), String>
 			match res.text().await {
 				Ok(body) => {
 					if let Ok(v) = serde_json::from_str::<Value>(&body) {
-						let user_id = v["id"].to_string();
-						let user_name = v["username"].as_str().unwrap().to_string();
+						let user_id = v[0]["id"].to_string().replace("\"","");
+						let user_name = v[0]["name"].to_string().replace("\"","");
 						Ok((user_id, user_name))
 					} else {
 						Err("Failed to get user's name".to_string())
@@ -193,14 +199,14 @@ struct RefreshState {
 async fn refresh_token(Json(msg_body): Json<RefreshState>) -> impl IntoResponse {
 
 	let params = [
-		("client_id", GITLAB_APP_CLIENT_ID.as_str()), 
-		("client_secret", GITLAB_APP_CLIENT_SECRET.as_str()),
+		("client_id", JIRA_APP_CLIENT_ID.as_str()), 
+		("client_secret", JIRA_APP_CLIENT_SECRET.as_str()),
 	    ("grant_type", "refresh_token"),
 		("refresh_token", &decrypt(&msg_body.refresh_state)),
 	];
 
 	let response = HTTP_CLIENT
-		.post("https://gitlab.com/oauth/token")
+		.post("https://auth.atlassian.com/oauth/token")
 		.form(&params)
 		.send()
 		.await;
@@ -229,7 +235,7 @@ async fn refresh_token(Json(msg_body): Json<RefreshState>) -> impl IntoResponse 
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ProjectName {
-	id: u64,
+	id: String,
 	name: String,
 }
 
@@ -238,9 +244,10 @@ struct RouteReq {
 	state: String,
 }
 
-async fn get_projects(token: &str) -> Result<Vec<ProjectName>, String> {
+async fn get_projects(token: &str, id: &str) -> Result<Vec<ProjectName>, String> {
 	let response = HTTP_CLIENT
-		.get(format!("https://gitlab.com/api/v4/projects?access_token={}&owned=true",token))
+		.get(format!("https://api.atlassian.com/ex/jira/{}/rest/api/latest/project",id))
+		.bearer_auth(token)
 		.send()
 		.await;
 	if let Ok(r) = response {
@@ -251,18 +258,19 @@ async fn get_projects(token: &str) -> Result<Vec<ProjectName>, String> {
 	Err("Failed to get channels".to_string())
 }
 
-async fn projects(Json(body): Json<RouteReq>) -> impl IntoResponse {
-	let authstate = decrypt(&body.state);
-	let mut token = body.state;
+async fn projects(Json(body): Json<Value>) -> impl IntoResponse {
+
+	let mut token = "".to_string();
+	let mut id = "".to_string();
 
 
-	if let Ok(v) = serde_json::from_str::<Value>(&authstate) {
+	if let Ok(v) = serde_json::from_str::<Value>(body["state"].as_str().unwrap()) {
 		token = v["access_token"].as_str().unwrap().to_string();
+		id = v["id"].as_str().unwrap().to_string();
 	}
 
-	// println!("{}",token);
 
-	match get_projects(&token).await {
+	match get_projects(&token,&id).await {
 		Ok(mut chs) => {
 			let rs: Vec<Value> = chs.iter_mut().filter_map(|ch| {
 				if true {
@@ -284,182 +292,62 @@ async fn projects(Json(body): Json<RouteReq>) -> impl IntoResponse {
 	}
 }
 
-#[derive(Debug, Deserialize)]
-struct FieldValue {
-	// field: String,
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ForwardRoute {
+	route: String,
 	value: String,
 }
-#[derive(Debug, Deserialize)]
-struct HookEvents {
-	events: Vec<FieldValue>,
-}
-#[derive(Debug, Deserialize)]
-struct HookReq {
+
+#[derive(Deserialize, Serialize)]
+struct PostBody {
 	user: String,
-    state: String,
-	field: String,
-	value: String,
-	flow: Option<String>,
-	custom: Option<HookEvents>,
+	text: String,
+	state: String,
+	forwards: Vec<ForwardRoute>,
 }
 
-async fn hook_events() -> impl IntoResponse {
-	let events = serde_json::json!({
-		"list": [
-			{
-				"field": "push",
-				"value": "push"
-			},
-			{
-				"field": "issue",
-				"value": "issue"
-			}
-		]
-	});
-	Json(events)
-}
-
-#[derive(Serialize, Deserialize)]
-struct OAuthState {
-	access_token: String,
-}
-
-async fn create_hook(Json(req): Json<HookReq>) -> impl IntoResponse {
-	if req.custom.is_none() || req.flow.is_none() {
-		return Err((StatusCode::BAD_REQUEST, "Not enough parameter".to_string()));
-	}
-
-	let auth_state = serde_json::from_str::<OAuthState>(&decrypt(&req.state)).unwrap();
-
-	let events: Vec<String> = req.custom.unwrap().events.iter().map(|e| {e.value.clone()}).collect();
-			
-	// println!("{}",auth_state.access_token);
-	// println!("{:?}",events);
-
-	match create_hook_inner(&req.user, &req.flow.unwrap(), &req.field, events, &auth_state.access_token, &req.value).await {
-		Ok(v) => {
-			Ok((StatusCode::CREATED, Json(v)))
-		}
-		Err(err_msg) => {
-			Err((StatusCode::INTERNAL_SERVER_ERROR, err_msg))
-		}
-	}
-}
-
-async fn create_hook_inner(connector: &str, flow_id: &str, repo_full_name: &str, events: Vec<String>, token: &str, id: &str) -> Result<Value, String> {
-	let param = json!({
-		"id": id,
-		"url": format!("{}/event?connector={connector}&flow={flow_id}",SERVICE_API_PREFIX.as_str()),
-		"token": token,
-		"push_events": true,
-		"tag_push_events": true,
-		"merge_requests_events": true,
-		"issues_events": true
-	});
-	let response = HTTP_CLIENT
-		.post(format!("https://gitlab.com/api/v4/projects/{}/hooks",id))
-		.header("Accept", "application/json")
-		.bearer_auth(token)
-		.json(&param)
-		.send()
-		.await;
-	if let Ok(r) = response {
-		if r.status().is_success() {
-			if let Ok(body) = r.bytes().await {
-				let json_body: Value = serde_json::from_slice(&body).unwrap();
-				let hook_id = json_body["id"].to_string();
-				let result = serde_json::json!({
-					"revoke": format!("{}/revoke-hook?hook_id={hook_id}&project={id}", SERVICE_API_PREFIX.as_str()),
-				});
-				return Ok(result);
-			}
-		}
-	}
-	Err("Failed to create hook".to_string())
-}
-
-#[derive(Deserialize)]
-struct EventQuery {
-	connector: String,
-	flow: String
-}
-
-async fn capture_event(Json(event): Json<Value>, Query(query): Query<EventQuery>) -> impl IntoResponse {
-	// println!("{:?} {} {}",event,query.connector,query.flow);
-	let connector = query.connector;
-	let flow = query.flow;
-	tokio::spawn(capture_event_inner(event,connector,flow));
-	(StatusCode::OK, String::new())
-}
-
-async fn capture_event_inner(event: Value, connector: String, flow: String) {
-	let te = event["object_kind"].as_str().unwrap();
-
-	let mut project: Value = serde_json::from_str(&event["project"].to_string()).unwrap();
-
-	let triggers = serde_json::json!({
-		"events": te,
-		"projects": project["id"],
-	});
-
-	// println!("{} {} {} {}",&connector, &flow, &event.to_string(), triggers);
-
-	post_event_to_reactor(&connector, &flow, &event.to_string(), triggers).await;
-
-}
-
-async fn post_event_to_reactor(user: &str, flow: &str, text: &str, triggers: Value) {
-	let request = serde_json::json!({
-		"user": user,
-		"flow": flow,
-		"text": text,
-		"triggers": triggers,
-	});
-
-	let response = HTTP_CLIENT
-		.post(format!("{}/api/_funcs/_post", REACTOR_API_PREFIX.as_str()))
-		.header(header::AUTHORIZATION, REACTOR_AUTH_TOKEN.as_str())
-		.json(&request)
-		.send()
-		.await;
-	if let Err(e) = response {
-		println!("{:?}", e);
-	}
-}
-
-#[derive(Deserialize)]
-struct RevokeQuery {
-	hook_id: String,
-	project: String
-}
-
-#[derive(Debug, Deserialize)]
-struct RevokeHookReq {
-    state: String
-}
-
-async fn revoke_hook(Json(req): Json<RevokeHookReq>, Query(query): Query<RevokeQuery>) -> impl IntoResponse {
-	let auth_state = serde_json::from_str::<OAuthState>(&decrypt(&req.state)).unwrap();
-
-	match revoke_hook_inner(&query.project, &query.hook_id, &auth_state.access_token).await {
-		Ok(()) => {
-			Ok(StatusCode::OK)
-		}
-		Err(err_msg) => {
-			Err((StatusCode::INTERNAL_SERVER_ERROR, err_msg))
-		}
-	}
-}
-
-async fn revoke_hook_inner(project: &str, hook_id: &str, token: &str) -> Result<(), String> {
-	let response = HTTP_CLIENT
-		.delete(format!("https://gitlab.com/api/v4/projects/{}/hooks/{}",project,hook_id))
-		.bearer_auth(token)
-		.send()
-		.await;
+async fn post_msg(Json(msg_body): Json<PostBody>) -> Result<StatusCode, (StatusCode, &'static str)> {
+	println!("{}",msg_body.text);
 	
-	return Ok(());
+	tokio::spawn(async move {
+		for pb in msg_body.forwards.iter() {
+			if pb.route.eq("projects") {
+				let request = serde_json::json!({
+					"fields": {
+					   "project":
+					   {
+						  "id": pb.value
+					   },
+					   "summary": msg_body.text,
+					   "description": "",
+					   "issuetype": {
+						  "id": "10001"
+					   }
+				   }
+				}
+				);
+
+				let mut token = "".to_string();
+				let mut id = "".to_string();
+
+				if let Ok(v) = serde_json::from_str::<Value>(msg_body.state.as_str()) {
+					token = v["access_token"].as_str().unwrap().to_string();
+					id = v["id"].as_str().unwrap().to_string();
+				}
+	
+				tokio::spawn(HTTP_CLIENT
+					.post(format!("https://api.atlassian.com/ex/jira/{}/rest/api/latest/issue",id))
+					.bearer_auth(token)
+					.json(&request)
+					.send());
+			}
+		}
+	});
+
+	Ok(StatusCode::OK)
 }
+
 
 #[tokio::main]
 async fn main() {
@@ -467,10 +355,7 @@ async fn main() {
 		.route("/auth", get(auth))
 		.route("/refresh", post(refresh_token))
 		.route("/projects", post(projects))
-		.route("/create-hook", post(create_hook))
-		.route("/revoke-hook", delete(revoke_hook))
-		.route("/hook-events", post(hook_events))
-		.route("/event", post(capture_event));
+		.route("/post", post(post_msg));
 
 	let port = env::var("PORT").unwrap_or_else(|_| "8090".to_string());
 	let port = port.parse::<u16>().unwrap();
