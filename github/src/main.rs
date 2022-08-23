1a71a1a6c6b85582bec1ef74c9d5252059630de8
DarumaDocker <DarumaDocker@gmail.com>
2022-08-23 15:58:41 +0800

    github - change copy

--- github/src/main.rs
+++ github/src/main.rs
@@ -650,20 +650,24 @@ async fn actions() -> impl IntoResponse {
     let events = serde_json::json!({
         "list": [
             {
-                "field": "Create Issue",
+                "field": "To create an Issue",
                 "value": "create-issue",
+                "desc": "This connector takes the return value of the flow function to create a new GitHub issue. It corresponds to the create-issue call in the GitHub API."
             },
             {
-                "field": "Create Issue/PR Comment",
+                "field": "To create a comment",
                 "value": "create-comment",
+                "desc": "This connector takes the return value of the flow function to create a new GitHub comment for a GitHub issue or Pull Request. It corresponds to the create-comment call in the GitHub API."
             },
             {
-                "field": "Add labels to an Issue/PR",
+                "field": "To add labels",
                 "value": "add-labels",
+                "desc": "This connector takes the return value of the flow function to add any number of existing label to a GitHub issue or Pull Request. It corresponds to the add-labels call in the GitHub API."
             },
             {
-                "field": "Add assignees to an Issue/PR",
+                "field": "To add assignees",
                 "value": "add-assignees",
+                "desc": "This connector takes the return value of the flow function to add any number of assignees for a GitHub issue or Pull Request. It corresponds to the add-assignees call in the GitHub API."
             },
         ],
     });
