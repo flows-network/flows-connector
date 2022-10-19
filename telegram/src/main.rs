18abe89ec621e206928f49c9f25139ab897508e4
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-10-19 15:48:03 +0800

    refactor telegram-connector

--- telegram/src/main.rs
+++ telegram/src/main.rs
@@ -2,75 +2,41 @@ use axum::{
     extract::{Json, Query},
     http::{header, HeaderMap, StatusCode},
     response::IntoResponse,
-    routing::{delete, get, post},
+    routing::{get, post},
     Router,
 };
-use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 use lazy_static::lazy_static;
-use rand::SeedableRng;
-use rand_chacha::ChaCha8Rng;
-use reqwest::{Client, ClientBuilder};
-use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+use reqwest::{Client, ClientBuilder, Method, RequestBuilder};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use std::env;
-use std::net::SocketAddr;
 use std::time::Duration;
-use urlencoding;
+use std::{collections::HashMap, env};
+use std::{fmt::Display, net::SocketAddr};
 
 const TIMEOUT: u64 = 120;
 
-const RSA_BITS: usize = 2048;
-
 lazy_static! {
-    static ref REACTOR_API_PREFIX: String = env::var("REACTOR_API_PREFIX").expect("Env variable REACTOR_API_PREFIX not set");
-    static ref REACTOR_AUTH_TOKEN: String = env::var("REACTOR_AUTH_TOKEN").expect("Env variable REACTOR_AUTH_TOKEN not set");
-
-    static ref TELEGRAM_BOT_TOKEN: String = env::var("TELEGRAM_BOT_TOKEN").expect("Env var TELEGRAM_BOT_TOKEN not set");
-    static ref TELEGRAM_BOT_NAME: String = env::var("TELEGRAM_BOT_NAME").expect("Env var TELEGRAM_BOT_NAME not set");
-
-    static ref SERVICE_API_PREFIX: String = env::var("SERVICE_API_PREFIX").expect("Env var SERVICE_API_PREFIX not set");
-
-    // static ref RSA_RAND_SEED: [u8; 32] = env::var("RSA_RAND_SEED")
-    //     .expect("Env variable RSA_RAND_SEED not set")
-    //     .as_bytes()
-    //     .try_into()
-    //     .unwrap();
-    static ref RSA_RAND_SEED: [u8; 32] = [8;32];
-
-    static ref CHACHA8RNG: ChaCha8Rng = ChaCha8Rng::from_seed(*RSA_RAND_SEED);
-    static ref PRIV_KEY: RsaPrivateKey =
-        RsaPrivateKey::new(&mut CHACHA8RNG.clone(), RSA_BITS).expect("failed to generate a key");
-    static ref PUB_KEY: RsaPublicKey = RsaPublicKey::from(&*PRIV_KEY);
-
+    static ref HAIKU_API_PREFIX: String =
+        env::var("HAIKU_API_PREFIX").expect("Env variable HAIKU_API_PREFIX not set");
+    static ref HAIKU_AUTH_TOKEN: String =
+        env::var("HAIKU_AUTH_TOKEN").expect("Env variable HAIKU_AUTH_TOKEN not set");
+    static ref TELEGRAM_BOT_TOKEN: String =
+        env::var("TELEGRAM_BOT_TOKEN").expect("Env var TELEGRAM_BOT_TOKEN not set");
+    static ref TELEGRAM_BOT_NAME: String =
+        env::var("TELEGRAM_BOT_NAME").expect("Env var TELEGRAM_BOT_NAME not set");
+    static ref SERVICE_API_PREFIX: String =
+        env::var("SERVICE_API_PREFIX").expect("Env var SERVICE_API_PREFIX not set");
+    static ref RSA_RAND_SEED: [u8; 32] = env::var("RSA_RAND_SEED")
+        .expect("Env variable RSA_RAND_SEED not set")
+        .as_bytes()
+        .try_into()
+        .unwrap();
+    static ref CONNECT_HTML: String =
+        include_str!("./connect.html").replace("{TELEGRAM_BOT_NAME}", &*TELEGRAM_BOT_NAME);
     static ref HTTP_CLIENT: Client = ClientBuilder::new()
         .timeout(Duration::from_secs(TIMEOUT))
         .build()
         .expect("Can't build the reqwest client");
-}
-
-fn encrypt(data: &str) -> String {
-    hex::encode(
-        PUB_KEY
-            .encrypt(
-                &mut CHACHA8RNG.clone(),
-                PaddingScheme::new_pkcs1v15_encrypt(),
-                data.as_bytes(),
-            )
-            .expect("failed to encrypt"),
-    )
-}
-
-fn decrypt(data: &str) -> String {
-    String::from_utf8(
-        PRIV_KEY
-            .decrypt(
-                PaddingScheme::new_pkcs1v15_encrypt(),
-                &hex::decode(data).unwrap(),
-            )
-            .expect("failed to decrypt"),
-    )
-    .unwrap()
 }
 
 #[derive(Deserialize, Serialize)]
@@ -80,271 +46,323 @@ struct AuthBody {
 }
 
 async fn auth(Query(auth_body): Query<AuthBody>) -> impl IntoResponse {
-    let id = auth_body.id;
-    let name = auth_body.name;
-    if id == "" {
-        Err((StatusCode::BAD_REQUEST, "No id or name".to_string()))
-    } else {
-        let location = format!(
-            "{}/api/connected?authorId={}&authorName={}&authorState={}",
-            REACTOR_API_PREFIX.as_str(),
-            id,
-            name,
-            encrypt(&id)
-        );
-        Ok((StatusCode::FOUND, [("Location", location)]))
-    }
+    let location = format!(
+        "{}/api/connected?authorId={}&authorName={}&authorState=none",
+        HAIKU_API_PREFIX.as_str(),
+        auth_body.id,
+        auth_body.name
+    );
+
+    (StatusCode::FOUND, [("Location", location)])
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct ProjectName {
-    id: String,
-    name: String,
+fn bot_api<U: AsRef<str>>(method: Method, uri: U) -> RequestBuilder {
+    HTTP_CLIENT.request(
+        method,
+        format!(
+            "https://api.telegram.org/bot{}{}",
+            &*TELEGRAM_BOT_TOKEN,
+            uri.as_ref()
+        ),
+    )
 }
 
-async fn chat(Json(body): Json<Value>) -> impl IntoResponse {
-    let events = serde_json::json!({
-        "list": [
-            {
-                "field": TELEGRAM_BOT_NAME.as_str(),
-                "value": TELEGRAM_BOT_NAME.as_str()
-            }
-        ]
-    });
-    Json(events)
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct ForwardRoute {
-    route: String,
+#[derive(Deserialize)]
+struct RouteItem {
+    // field: String,
     value: String,
 }
 
-#[derive(Deserialize, Serialize)]
-struct PostBody {
-    user: String,
+#[derive(Deserialize)]
+struct Route {
+    action: Vec<RouteItem>,
+}
+
+#[derive(Deserialize)]
+struct HaikuReqBody {
+    // user: String,
     text: String,
-    state: String,
-    forwards: Vec<ForwardRoute>,
+    forwards: Route,
 }
 
-async fn post_msg(
-    Json(msg_body): Json<PostBody>,
-) -> Result<StatusCode, (StatusCode, &'static str)> {
-    println!("{}",msg_body.text);
+async fn post_msg(req: Json<HaikuReqBody>) -> Result<(), (StatusCode, String)> {
+    let action = &req
+        .forwards
+        .action
+        .first()
+        .ok_or((StatusCode::BAD_REQUEST, "Missing action item".to_string()))?
+        .value;
 
-    let words: Vec<&str> = msg_body.text.split(" ").collect();
-
-    if words[0] == "ban" {
-        let words: Vec<&str> = msg_body.text.split(" ").collect();
-        let text = format!("ban user: {} \nfrom group: {}", words[4], words[3]);
-
-        tokio::spawn(
-            HTTP_CLIENT
-                .post(format!(
-                    "https://api.telegram.org/bot{}/banChatMember?chat_id={}&user_id={}",
-                    TELEGRAM_BOT_TOKEN.as_str(),
-                    words[1],
-                    words[2]
-                ))
-                .send(),
-        );
-
-        tokio::spawn(
-            HTTP_CLIENT
-                .post(format!(
-                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}",
-                    TELEGRAM_BOT_TOKEN.as_str(),
-                    msg_body.user.as_str(),
-                    urlencoding::encode(text.as_str())
-                ))
-                .send(),
-        );
-    } else if words[0] == "welcome" {
-        let words: Vec<&str> = msg_body.text.split(" ").collect();
-        let mut text = format!("[{}, welcome\\!](tg://user?id={})", words[3], words[2]);
-        text = text.replace("_", "\\_");
-
-        tokio::spawn(
-            HTTP_CLIENT
-                .post(format!(
-                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=MarkdownV2",
-                    TELEGRAM_BOT_TOKEN.as_str(),
-                    words[1],
-                    text.as_str()
-                ))
-                .send(),
-        );
-    }else if words[0] == "warning" {
-        let words: Vec<&str> = msg_body.text.split(" ").collect();
-        let text = format!("[Warning\\! Can't send strange url in this group\\!](tg://user?id={})", words[2]);
-        tokio::spawn(
-            HTTP_CLIENT
-                .post(format!(
-                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=MarkdownV2",
-                    TELEGRAM_BOT_TOKEN.as_str(),
-                    words[1],
-                    text.as_str()
-                ))
-                .send(),
-        );
+    let response = match action.as_str() {
+        "send" => bot_api(Method::POST, "/sendMessage"),
+        "ban" => bot_api(Method::POST, "/banChatMember"),
+        "unban" => bot_api(Method::POST, "/unbanChatMember"),
+        _ => return Err((StatusCode::BAD_REQUEST, "Unsupported action".to_string())),
     }
-    else {
-        tokio::spawn(
-            HTTP_CLIENT
-                .post(format!(
-                    "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}",
-                    TELEGRAM_BOT_TOKEN.as_str(),
-                    msg_body.user.as_str(),
-                    urlencoding::encode(msg_body.text.as_str())
-                ))
-                .send(),
-        );
-    }
+    .header(header::CONTENT_TYPE, "application/json")
+    .body(req.text.to_owned())
+    .send()
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    Ok(StatusCode::OK)
+    match response.status().is_success() {
+        true => Ok(()),
+        false => Err((response.status(), response.text().await.unwrap_or_default())),
+    }
 }
-
-static CONNECT_HTML: &str = include_str!("./connect.html");
 
 async fn connect() -> impl IntoResponse {
     (
         StatusCode::OK,
         [("Content-Type", "text/html; charset=utf-8")],
-        CONNECT_HTML,
+        CONNECT_HTML.as_str(),
     )
 }
 
-#[derive(Debug, Deserialize)]
-struct HookReq {
-    user: String,
-    state: String,
-    field: String,
-    value: String,
-    flow: Option<String>,
+#[derive(Deserialize)]
+struct ApiResult<R> {
+    // ok: bool,
+    #[serde(bound(deserialize = "R: Deserialize<'de>"))]
+    result: R,
 }
 
-async fn create_hook(Json(req): Json<HookReq>) -> impl IntoResponse {
-    match create_hook_inner(&req.user, &req.flow.unwrap(), &req.field, &req.value).await {
-        Ok(v) => Ok((StatusCode::CREATED, Json(v))),
-        Err(err_msg) => Err((StatusCode::INTERNAL_SERVER_ERROR, err_msg)),
-    }
+#[derive(Deserialize)]
+struct Member {
+    user: User,
+    status: String,
 }
 
-async fn create_hook_inner(
-    connector: &str,
-    flow_id: &str,
-    repo_full_name: &str,
-    id: &str,
-) -> Result<Value, String> {
-    tokio::spawn(
-        HTTP_CLIENT
-            .post(format!(
-                "https://api.telegram.org/bot{}/setWebhook?url={}/newmessage&secret_token={}",
-                TELEGRAM_BOT_TOKEN.as_str(),
-                SERVICE_API_PREFIX.as_str(),
-                format!("{}_{}", connector, flow_id)
-            ))
-            .send(),
-    );
-
-    let result = serde_json::json!({
-        "revoke": format!("{}/revoke-hook", SERVICE_API_PREFIX.as_str()),
-    });
-
-    return Ok(result);
+#[derive(Deserialize)]
+struct User {
+    id: i64,
 }
 
-async fn revoke_hook() -> impl IntoResponse {
-    match revoke_hook_inner().await {
-        Ok(()) => Ok(StatusCode::OK),
-        Err(err_msg) => Err((StatusCode::INTERNAL_SERVER_ERROR, err_msg)),
-    }
+async fn get_chat_owner<C: Display>(chat_id: C) -> Result<User, String> {
+    bot_api(
+        Method::GET,
+        format!("/getChatAdministrators?chat_id={chat_id}"),
+    )
+    .send()
+    .await
+    .map_err(|e| e.to_string())?
+    .json::<ApiResult<Vec<Member>>>()
+    .await
+    .map_err(|e| e.to_string())?
+    .result
+    .into_iter()
+    .find(|member| member.status.eq("creator"))
+    .ok_or("Can not find owner".to_string())
+    .map(|member| member.user)
 }
 
-async fn revoke_hook_inner() -> Result<(), String> {
-    // let response = HTTP_CLIENT
-    //     .post(format!(
-    //         "https://api.telegram.org/bot{}/deleteWebhook?drop_pending_updates=True",
-    //         TELEGRAM_BOT_TOKEN.as_str()
-    //     ))
-    //     .send()
-    //     .await;
-
-    return Ok(());
+#[derive(Deserialize)]
+struct WebhookPayload {
+    #[serde(rename = "update_id")]
+    _update_id: i64,
+    #[serde(flatten)]
+    event: HashMap<String, Value>,
 }
 
-async fn newmessage(
-    Json(msg_body): Json<Value>,
+async fn capture_event(
+    payload: Json<WebhookPayload>,
     headers: HeaderMap,
-) -> Result<StatusCode, (StatusCode, &'static str)> {
-    println!("{:?}",msg_body);
+) -> Result<StatusCode, StatusCode> {
+    headers
+        .get("X-Telegram-Bot-Api-Secret-Token")
+        .ok_or(StatusCode::UNAUTHORIZED)?
+        .eq(&*TELEGRAM_BOT_NAME)
+        .then_some(())
+        .ok_or(StatusCode::UNAUTHORIZED)?;
 
-    let words: Vec<&str> = headers["x-telegram-bot-api-secret-token"]
-        .to_str()
-        .unwrap()
-        .split("_")
-        .collect();
-
-    let connector = words[0];
-    let flow = words[1];
-
-    tokio::spawn(capture_event_inner(
-        msg_body,
-        connector.to_string(),
-        flow.to_string(),
-    ));
+    if let Err(e) = capture_event_inner(
+        payload
+            .event
+            .to_owned()
+            .into_iter()
+            .next()
+            .ok_or(StatusCode::BAD_REQUEST)?,
+    )
+    .await
+    {
+        println!("capture event error: {}", e);
+    }
 
     Ok(StatusCode::OK)
 }
 
-async fn capture_event_inner(event: Value, connector: String, flow: String) {
-    let mut project: Value = serde_json::from_str(&event["message"].to_string()).unwrap();
-
-    let triggers = serde_json::json!({
-        "chat": TELEGRAM_BOT_NAME.as_str(),
-    });
-
-    post_event_to_reactor(&connector, &flow, &event.to_string(), triggers).await;
+#[derive(Deserialize, Debug)]
+struct Chat {
+    id: i64,
+    r#type: String,
 }
 
-async fn post_event_to_reactor(user: &str, flow: &str, text: &str, triggers: Value) {
-    let request = serde_json::json!({
-        "user": user,
-        "flow": flow,
-        "text": text,
-        "triggers": triggers,
-    });
+#[derive(Deserialize, Debug)]
+struct SupportedEvent {
+    chat: Chat,
+}
 
+async fn capture_event_inner(event: (String, Value)) -> Result<(), String> {
+    let chat = match serde_json::from_value::<SupportedEvent>(event.1.clone()) {
+        Ok(c) => c.chat,
+        Err(_) => return Ok(()),
+    };
+
+    let user = match chat.r#type.as_str() {
+        "private" => chat.id,
+        _ => {
+            get_chat_owner(chat.id)
+                .await
+                .map_err(|e| format!("get_chat_owner failed: {}", e.to_string()))?
+                .id
+        }
+    }
+    .to_string();
+
+    post_event_to_haiku(
+        user,
+        json!({ event.0.clone(): event.1 }).to_string(),
+        [("event", event.0)],
+    )
+    .await
+}
+
+async fn post_event_to_haiku<
+    U: Into<String>,
+    T: Into<String>,
+    TK: Into<String>,
+    TV: Into<String>,
+    TRI: IntoIterator<Item = (TK, TV)>,
+>(
+    user: U,
+    text: T,
+    triggers: TRI,
+) -> Result<(), String> {
     let response = HTTP_CLIENT
-        .post(format!("{}/api/_funcs/_post", REACTOR_API_PREFIX.as_str()))
-        .header(header::AUTHORIZATION, REACTOR_AUTH_TOKEN.as_str())
-        .json(&request)
+        .post(format!("{}/api/_funcs/_post", HAIKU_API_PREFIX.as_str()))
+        .header(header::AUTHORIZATION, HAIKU_AUTH_TOKEN.as_str())
+        .json(&json!({
+            "user": user.into(),
+            "text": text.into(),
+            "triggers": triggers
+                .into_iter()
+                .map(|(k, v)| (k.into(), v.into()))
+                .collect::<HashMap<_, _>>()
+        }))
         .send()
-        .await;
+        .await
+        .map_err(|e| format!("Failed to post event to haiku: {}", e.to_string()))?;
 
-    if let Err(e) = response {
-        println!("{:?}", e);
+    match response.status().is_success() {
+        true => Ok(()),
+        false => Err(format!(
+            "Failed to post event to haiku: {}",
+            response.text().await.unwrap_or_default()
+        )),
+    }
+}
+
+async fn actions() -> impl IntoResponse {
+    Json(json!({
+        "list": [
+            {
+                "field": "Send a message to chat",
+                "value": "send"
+            },
+            {
+                "field": "Ban a user in a chat",
+                "value": "ban"
+            },
+            {
+                "field": "Unban a previously banned user in a chat",
+                "value": "unban"
+            }
+        ]
+    }))
+}
+
+async fn events() -> impl IntoResponse {
+    Json(json!({
+        "list": [
+            {
+                "field": "New incoming message",
+                "value": "message",
+                "desc": "New incoming message of any kind - text, photo, sticker, etc."
+            },
+            {
+                "field": "New version of a edited message",
+                "value": "edited_message",
+                "desc": "New version of a message that is known to the bot and was edited."
+            },
+            {
+                "field": "New incoming channel post",
+                "value": "channel_post",
+                "desc": "New incoming channel post of any kind - text, photo, sticker, etc."
+            },
+            {
+                "field": "New version of a edited channel post",
+                "value": "edited_channel_post",
+                "desc": "New version of a channel post that is known to the bot and was edited"
+            },
+            {
+                "field": "Bot's chat member status was updated in a chat",
+                "value": "my_chat_member",
+                "desc": "The bot's chat member status was updated in a chat. For private chats, this update is received only when the bot is blocked or unblocked by the user."
+            },
+            {
+                "field": "A chat member's status was updated in a chat",
+                "value": "chat_member",
+                "desc": "A chat member's status was updated in a chat. The bot must be an administrator in the chat."
+            },
+            {
+                "field": "A request to join the chat has been sent",
+                "value": "chat_join_request",
+                "desc": "A request to join the chat has been sent."
+            },
+        ]
+    }))
+}
+
+async fn init_webhook() -> Result<(), String> {
+    let response = bot_api(
+        Method::POST,
+        format!(
+            "/setWebhook?url={}/webhook&secret_token={}&drop_pending_updates=true",
+            &*SERVICE_API_PREFIX, &*TELEGRAM_BOT_NAME
+        ),
+    )
+    .send()
+    .await
+    .map_err(|e| format!("setWebhook failed: {}", e.to_string()))?;
+
+    match response.status().is_success() {
+        true => Ok(()),
+        false => Err(format!(
+            "setWebhook failed: {}",
+            response.text().await.unwrap_or_default()
+        )),
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .route("/auth", get(auth))
         .route("/connect", get(connect))
-        .route("/chat", post(chat))
         .route("/post", post(post_msg))
-        .route("/revoke-hook", delete(revoke_hook))
-        .route("/create-hook", post(create_hook))
-        .route("/newmessage", post(newmessage));
+        .route("/actions", post(actions))
+        .route("/events", post(events))
+        .route("/webhook", post(capture_event));
 
-    let port = env::var("PORT").unwrap_or_else(|_| "8090".to_string());
-    let port = port.parse::<u16>().unwrap();
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8090".to_string())
+        .parse::<u16>()?;
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    init_webhook().await?;
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
